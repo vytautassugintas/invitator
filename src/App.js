927980976f30f6bde31fd1abc748df8a219d7e6a
Vytautas Sugintas <vytautas.sugintas@gmail.com>
2018-01-01 22:38:40 +0200

    feat: first konva shape

--- src/App.js
+++ src/App.js
@@ -1,21 +1,43 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { render } from "react-dom";
+import Konva from "konva";
+import { Stage, Layer, Rect, Text } from "react-konva";
 
-class App extends Component {
+class ColoredRect extends React.Component {
+  state = {
+    color: "green"
+  };
+
+  handleClick = () => {
+    this.setState({
+      color: Konva.Util.getRandomColor()
+    });
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Rect
+        x={20}
+        y={20}
+        width={50}
+        height={50}
+        fill={this.state.color}
+        shadowBlur={5}
+        onClick={this.handleClick}
+      />
     );
   }
 }
 
-export default App;
+export default class App extends Component {
+  render() {
+    return (
+      <Stage width={window.innerWidth} height={window.innerHeight}>
+        <Layer>
+          <Text text="Konva rect test" />
+          <ColoredRect />
+        </Layer>
+      </Stage>
+    );
+  }
+}
